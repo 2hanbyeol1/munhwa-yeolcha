3a54a410471fe02a33a7ccf407e3a4b54369d920
2hanbyeol1 <2hanbyeol1@naver.com>
2024-07-09 00:33:36 +0900

    Chore: zustand 설정 및 테스트

--- src/app/(provider)/(main)/page.tsx
+++ src/app/(provider)/(main)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { getTest } from "@/axios/axios.test";
+import useCountStore from "@/zustand/count.store";
 import { useQuery } from "@tanstack/react-query";
 
 type DataType = {
@@ -12,11 +13,21 @@ const MainPage = () => {
     queryFn: () => getTest()
   });
 
+  const { count, increment, decrement } = useCountStore();
+
   if (isPending) return <>pending</>;
   if (isError) return <>error</>;
   console.log(data);
 
-  return <main>{data.title}</main>;
+  return (
+    <main>
+      {data.title}
+      <hr />
+      <div>{count}</div>
+      <button onClick={increment}>[ + ]</button>
+      <button onClick={decrement}>[ - ]</button>
+    </main>
+  );
 };
 
 export default MainPage;
