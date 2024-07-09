807fb5eed991772f71a23c0f7834b7421fe7adb9
2hanbyeol1 <2hanbyeol1@naver.com>
2024-07-09 15:42:12 +0900

    Design: swiper 적용

--- src/app/(provider)/(main)/page.tsx
+++ src/app/(provider)/(main)/page.tsx
@@ -1,13 +1,11 @@
-import Film from "@/components/Film";
+import FilmSwiper from "@/components/FilmSwiper";
 import Show from "@/components/Show";
 
 const MainPage = () => {
   return (
     <>
-      <section className="w-full grid grid-cols-3">
-        <Film image="https://mir-s3-cdn-cf.behance.net/project_modules/hd/fdef9c31941083.566793886985a.png" />
-        <Film image="https://mir-s3-cdn-cf.behance.net/project_modules/hd/fdef9c31941083.566793886985a.png" />
-        <Film image="https://mir-s3-cdn-cf.behance.net/project_modules/hd/fdef9c31941083.566793886985a.png" />
+      <section>
+        <FilmSwiper />
       </section>
       <section className="mt-10 w-full grid grid-cols-4 gap-5">
         <Show image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwOJwaIGV12m9rDXUdIsetg0DuXVDZLWZhXA&s" />
