e145dfba2658fb6ddb724823bcfe3bced64734e3
RingKim1 <kgh9393@naver.com>
2024-07-09 16:33:02 +0900

    Design: 버튼 호버링시 이미지 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button = ({ buttonName, onClick }: ButtonProps) => {
   return (
     <button
-      className="bg-green rounded text-white font-semibold mx-1 my-2 py-1 px-4  hover:opacity-70 hover:text-black"
+      className="bg-green rounded text-white font-semibold mx-1 my-2 py-1 px-4  hover:bg-white hover:text-green"
       onClick={onClick}
     >
       {buttonName}
