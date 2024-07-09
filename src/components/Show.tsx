10e8d541605bb0487b2efee8cd2043a1927c447e
2hanbyeol1 <2hanbyeol1@naver.com>
2024-07-09 18:03:55 +0900

    Design: tailwind config 수정 및 영화 정보 스타일링

--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -6,11 +6,11 @@ interface ShowType {
 
 const Show = ({ image }: ShowType) => {
   return (
-    <div className="relative w-full aspect-poster">
-      <Image className="object-cover" src={image} fill alt="연극" />
-      <div className="max-w-40 absolute -bottom-3 -right-3 p-5 bg-withered-diamond-translucent rounded-lg shadow-md">
-        <div>탈연극</div>
-        <div>서울특별시</div>
+    <div className="relative w-full aspect-poster transition-transform hover:scale-110">
+      <Image sizes="100vw" className="w-full object-cover" src={image} alt="연극" fill />
+      <div className="flex flex-col max-w-40 absolute -bottom-3 -right-3 p-5 bg-withered-diamond-translucent rounded-lg shadow-md">
+        <span>탈연극</span>
+        <span className="text-sm text-gray-700">서울특별시</span>
       </div>
     </div>
   );
