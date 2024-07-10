4db262205247cffbfb8508b6f85331ffde6006b3
Seung Bin Lee <leesb5510@gmail.com>
2024-07-10 12:10:10 +0900

    Feat: UI 수정 완료

--- src/components/EditProfileForm.tsx
+++ src/components/EditProfileForm.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import TicketList from "./TicketList";
 // import { fetchUserProfile, updateUserProfile } from "";
 
 // interface ProfileFormProps {
@@ -7,7 +6,7 @@ import TicketList from "./TicketList";
 // }
 
 const EditProfileForm: React.FC = () => {
-  // const [profile, setProfile] = useState({ name: "", password: "" });
+  // const [profile, setProfile] = useState({ password: "" });
   // const [isLoading, setIsLoading] = useState(true);
   // const [error, setError] = useState<string | null>(null);
 
@@ -46,14 +45,6 @@ const EditProfileForm: React.FC = () => {
   return (
     <form className="ml-8 mr-8 space-y-4">
       <div className="flex">
-        <label className="bg-green text-white text-center text-sm py-2 px-3 w-32 h-9 rounded-l-md">이름</label>
-        <input
-          type="text"
-          name="name"
-          className="block border border-gray-300 py-2 px-1 w-full h-9 rounded-r-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div className="flex">
         <label className="bg-green text-white text-center py-2 px-3 w-32 h-9 text-sm rounded-l-md">비밀번호</label>
         <input
           type="password"
@@ -62,7 +53,9 @@ const EditProfileForm: React.FC = () => {
         />
       </div>
       <div className="flex">
-        <label className="bg-green text-white text-center text-xs py-2 px-3 w-32 h-9 rounded-l-md">비밀번호 확인</label>
+        <label className="bg-green text-white text-center text-xs py-2.5 px-2 w-32 h-9 rounded-l-md">
+          비밀번호 확인
+        </label>
         <input
           type="password"
           name="confirmPassword"
