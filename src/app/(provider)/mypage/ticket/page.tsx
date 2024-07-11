b126107d77b70bc762be0f78582251a634dda6b2
Kimsounggu <tjdrn4657@naver.com>
2024-07-11 11:16:13 +0900

    Fix: pull dev 후 충돌해결

--- src/app/(provider)/mypage/ticket/page.tsx
+++ src/app/(provider)/mypage/ticket/page.tsx
@@ -1,73 +1,126 @@
+"use client";
+import Button from "@/components/Button";
+import { createClient } from "@/supabase/client";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const userId = "7c65fc32-2be4-478d-ae6d-7ea8506de71b";
+
+// 날짜와 시간 동기화
+const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  if (isNaN(Number(date))) {
+    return "";
+  }
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const daysWeek = daysOfWeek[date.getDay()];
+  return `${year}-${month}-${day}(${daysWeek})`;
+};
+
+// 임시 타입지정
+export interface Ticket {
+  id: string;
+  created_at: string;
+  user_id: string;
+  post_id: string;
+  date: string;
+  title: string;
+  image_url: string;
+  status: string;
+}
 
 const MyTicketingListPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [sortOrder, setSortOrder] = useState<string>("예약일 순");
+  const [reserved, setReserved] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await createClient().from("reservation").select("*").eq("user_id", userId);
+      setTickets(data); //여기 타입 물어보기
+    };
+    fetchData();
+  }, []);
+
+  // select부분
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOrder(e.target.value);
+  };
+
+  const sortTickets = (tickets: Ticket[], sortOrder: string) => {
+    return tickets.slice().sort((reservation, day) => {
+      if (sortOrder === "예약일 순") {
+        return new Date(reservation.created_at).getTime() - new Date(day.created_at).getTime();
+      } else if (sortOrder === "공연 날짜 순") {
+        return new Date(reservation.date).getTime() - new Date(day.date).getTime();
+      }
+      return 0;
+    });
+  };
+
+  const sortedTickets = sortTickets(tickets, sortOrder);
+
+  // 제목 자르기
+  // const truncateTitle = (title: string) => {
+  //   return title.length > 5 ? `${title.slice(0, 5)}...` : title;
+  // };
+
+  // 1. 데이터 가져오기
+  // 2. 아이디와 아이디가 같은 걸 supabase에서 삭제하기
+  // 3. 화면에 보여주기
+  useEffect(() => {
+    const deleteData = async () => {
+      const { data: reservedData } = await createClient().from("reservation").delete().eq("user_id", userId);
+      if (reservedData) {
+        setReserved(false);
+      }
+
+      setReserved(reservedData); //여기 타입 물어보기
+    };
+
+    deleteData();
+  }, []);
+
   return (
     <>
-      {/* 여기 위에는 버튼 밑에는 전체 예약 내역*/}
       <span>
         <div className="flex justify-between gap-5 py-[10px]">
           <span className="flex justify-center items-center w-[200px] h-[50px] rounded-lg font-bold text-green text-[30px]">
             전체 예약 내역
           </span>
-          <select className="border rounded-md bg-white">
+          <select className="border rounded-md bg-white" onChange={handleSortChange} value={sortOrder}>
             <option>예약일 순</option>
             <option>공연 날짜 순</option>
           </select>
         </div>
       </span>
-      <div className="border-t border-b border-l-0 border-r-0 border-gray-300">
-        <div className="flex flex-col">
-          {/* 여기부터 예매내역칸 */}
-          <div className="flex items-center p-[20px] border-t border-gray-300">
-            <div>
-              <div>
-                <div>2024.07.01(월)</div>
-                <div className="text-[10px] text-[gray]">예약번호:PF132236</div>
+      <div className="border-t border-b border-l border-r-0 border-gray-400">
+        <div className="overflow-x-auto">
+          <div className="flex">
+            {sortedTickets.map((ticket) => (
+              <div key={ticket.id} className="flex items-center p-[20px] border-r border-gray-400 min-w-[400px]">
+                <div>
+                  <div>
+                    <div>공연 날짜 : {formatDate(ticket.date)}</div>
+                    <div className="text-[10px] text-[gray] pt-2 pb-3">예약번호:{ticket.post_id}</div>
+                  </div>
+                  <Link href={`/detail/${userId}`} className="border w-[100px]">
+                    <Image src={ticket.image_url} alt={ticket.title} width={150} height={100} />
+                  </Link>
+                </div>
+                <div className="flex flex-col ml-4">
+                  <div className="font-bold pb-[10px] text-[25px] truncate max-w-32">{ticket.title}</div>
+                  <div className="text-[10px] text-[gray] pb-1">예약 날짜 : {formatDate(ticket.created_at)}</div>
+                  <div>{ticket.status}</div>
+                  <button></button>
+                </div>
               </div>
-              <div className="border w-[100px]">
-                <Image src={""} alt="웃는 남자" width={100} height={100} />
-              </div>
-            </div>
-            <div className="flex flex-col ml-4">
-              <div className="font-bold pb-[10px]">웃는 남자</div>
-              <div className="text-[10px] text-[gray]">2024.07.10 18:00</div>
-              <div>예약되었습니다람쥐</div>
-            </div>
-          </div>
-
-          {/* 여기부터 예매내역칸 */}
-          <div className="flex items-center p-[20px] border-t border-gray-300">
-            <div>
-              <div>
-                <div>2024.07.01(월)</div>
-                <div className="text-[10px] text-[gray]">예약번호:D813VC4</div>
-              </div>
-              <div className="border w-[100px]">
-                <Image src={""} alt="웃는 남자" width={100} height={100} />
-              </div>
-            </div>
-            <div className="flex flex-col ml-4">
-              <div className="font-bold pb-[10px]">웃는 남자</div>
-              <div className="text-[10px] text-[gray]">2024.07.10 18:00</div>
-              <div>예약되었습니다람쥐</div>
-            </div>
-          </div>
-          <div className="flex items-center p-[20px] border-t border-gray-300">
-            <div>
-              <div>
-                <div>2024.07.01(월)</div>
-                <div className="text-[10px] text-[gray]">예약번호:D813VC4</div>
-              </div>
-              <div className="border w-[100px]">
-                <Image src={""} alt="웃는 남자" width={100} height={100} />
-              </div>
-            </div>
-            <div className="flex flex-col ml-4">
-              <div className="font-bold pb-[10px]">웃는 남자</div>
-              <div className="text-[10px] text-[gray]">2024.07.10 18:00</div>
-              <div>예약되었습니다람쥐</div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/TicketList.tsx
+++ src/components/TicketList.tsx
@@ -1,7 +1,14 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const TicketList = () => {
+  const [clickButton, setClickButton] = useState("ticket");
+
+  const handleLinkClick = (link: string) => {
+    setClickButton(link);
+  };
+
   return (
     <>
       <div className="flex justify-center items-center h-[600px] w-[100%]">
@@ -9,13 +16,19 @@ const TicketList = () => {
           <div className="flex flex-col justify-center">
             <Link
               href={"/mypage/ticket"}
-              className=" bg-[white] p-3 rounded-lg w-40 mb-1 font-bold text-green text-center"
+              className={`p-3 rounded-lg w-40 mb-1 font-bold text-center ${
+                clickButton === "ticket" ? "bg-white text-green" : "bg-green text-white"
+              }`}
+              onClick={() => handleLinkClick("ticket")}
             >
               예약 내역
             </Link>
             <Link
               href={"/mypage/edit"}
-              className="border bg-green p-3 rounded-lg w-40 mb-1 font-bold text-[white] text-center"
+              className={`p-3 rounded-lg w-40 mb-1 font-bold text-center ${
+                clickButton === "edit" ? "bg-white text-green" : "bg-green text-white"
+              }`}
+              onClick={() => handleLinkClick("edit")}
             >
               회원정보 수정
             </Link>
