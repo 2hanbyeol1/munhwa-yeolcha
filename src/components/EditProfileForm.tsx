3688d67d9839619628a7ab5b3273c2756ff9e239
Seung Bin Lee <leesb5510@gmail.com>
2024-07-12 21:54:47 +0900

    Feat: 비밀번호가 URL에 표시되는 경우를 수정

--- src/components/EditProfileForm.tsx
+++ src/components/EditProfileForm.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import { createClient } from "@/supabase/client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const EditProfileForm: React.FC = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const supabase = createClient();
 
-  const handleChangePassword = async () => {
+  const handleChangePassword = async (e: any) => {
+    e.preventDefault();
     if (confirmPassword.length < 6) return alert("비밀번호는 6글자 이상이어야 합니다.");
+    if (!confirm("정말 비밀번호를 변경하시겠습니까?")) {
+      return;
+    }
     if (newPassword === confirmPassword) {
-      const { error: updateError } = await supabase.auth.updateUser({ password: newPassword });
+      const { error: updateError } = await supabase.auth.updateUser({ password: confirmPassword });
       if (!updateError) {
         alert("비밀번호 변경이 완료되었습니다.");
+        setNewPassword("");
+        setConfirmPassword("");
       } else {
-        console.log(updateError.message);
+        alert(updateError.message);
       }
     } else {
       alert("비밀번호가 일치하는 지 다시 확인해주세요.");
@@ -28,7 +34,7 @@ const EditProfileForm: React.FC = () => {
         <label className="bg-green text-white text-center py-2 px-2 w-36 h-9 text-sm rounded-l-md">새 비밀번호</label>
         <input
           type="password"
-          name="password"
+          name="newPassword"
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
           className="block border border-gray-300 py-2 px-1 w-full h-9 rounded-r-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
